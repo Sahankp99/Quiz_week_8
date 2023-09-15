55e72a735233bf6f7568f42e542107d0dbe6e6a7
M_Awais_Saleem <awais.saleem@odysseyanalytics.net>
2023-09-15 15:53:11 +0500

    Added files and script to load data from CSV and plot graphs and logging of github commands

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,32 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
+
+conn = sqlite3.connect(r"climate.db")
+cursor = conn.cursor()
+
+sql_command = "SELECT * FROM ClimateData"
+cursor.execute(sql_command)
+result = cursor.fetchall()
+
 years = []
 co2 = []
 temp = []
 
+for row in result:
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+conn.close()
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_1.png")
--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+# Read csv file
+df = pd.read_csv('climate.csv')
 
 years = []
 co2 = []
 temp = []
 
+# loop to extract and save values in variables
+for index, row in df.iterrows():
+    years.append(int(row[0]))
+    co2.append(int(row[1]))
+    temp.append(row[2])
+
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
-
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
